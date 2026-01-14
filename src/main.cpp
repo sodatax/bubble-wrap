abbc45058b61c456feb2dcce494d9221c9cce023
sodatax <[200~62825932+sodatax@users.noreply.github.com~>
2026-01-14 04:16:42 +0000

    added new backdrops depending on keypress

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <bn_backdrop.h>
 #include <bn_color.h>
 #include <bn_core.h>
+#include <bn_keypad.h>
 
 int main(){
     bn::core::init();
@@ -8,5 +9,17 @@ int main(){
 
     while(true){
         bn::core::update();
+
+        if(bn::keypad::a_pressed()){
+            bn::backdrop::set_color(bn::color(24,24,31));
+        }
+
+        if(bn::keypad::b_pressed()){
+            bn::backdrop::set_color(bn::color(24,31,24));
+        }
+
+        if(bn::keypad::a_pressed() && bn::keypad::b_pressed()){
+            bn::backdrop::set_color(bn::color(31,24,24));
+        }
     }
 }

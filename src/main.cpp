f54c5ccf10bdf2147285ac142e7322a747cc7df5
sodatax <[200~62825932+sodatax@users.noreply.github.com~>
2026-01-14 03:59:34 +0000

    changed the backdrop to pastel blue

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <bn_backdrop.h>
+#include <bn_color.h>
+#include <bn_core.h>
+
+int main(){
+    bn::core::init();
+    bn::backdrop::set_color(bn::color(20,20,31));
+
+    while(true){
+        bn::core::update();
+    }
+}

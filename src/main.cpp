d38ab0864b6b093a028db58778d24df25ff3dc29
sodatax <[200~62825932+sodatax@users.noreply.github.com~>
2026-01-14 04:02:42 +0000

    changed backdrop to pastel red

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 int main(){
     bn::core::init();
-    bn::backdrop::set_color(bn::color(20,20,31));
+    bn::backdrop::set_color(bn::color(31,24,24));
 
     while(true){
         bn::core::update();

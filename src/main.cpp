1e3cc6387e9375dea093fe3235a9300312376d3a
sodatax <[200~62825932+sodatax@users.noreply.github.com~>
2026-01-19 23:31:31 +0000

    added a smiley face sprite

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,22 @@
 #include <bn_color.h>
 #include <bn_core.h>
 #include <bn_keypad.h>
+#include <bn_sprite_ptr.h>
+#include <bn_sprite_items_dot.h>
 
 int main(){
     bn::core::init();
     bn::backdrop::set_color(bn::color(31,24,24));
     int color = 0;
+
+    bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(0, 0);
+    bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(-20, 0);
+    bn::sprite_ptr myCircle3 = bn::sprite_items::dot.create_sprite(0, 30);
+    bn::sprite_ptr myCircle4 = bn::sprite_items::dot.create_sprite(-20, 30);
+    bn::sprite_ptr myCircle5 = bn::sprite_items::dot.create_sprite(-15, 35);
+    bn::sprite_ptr myCircle6 = bn::sprite_items::dot.create_sprite(-10, 35);
+    bn::sprite_ptr myCircle7 = bn::sprite_items::dot.create_sprite(-5, 35);
+
 
     while(true){
         bn::core::update();
@@ -30,7 +41,7 @@ int main(){
         }
         if(bn::keypad::b_pressed()){
             color = 2;
-        }
+        } 
         if(bn::keypad::a_pressed() && bn::keypad::b_pressed()){
             color = 3;
         }
